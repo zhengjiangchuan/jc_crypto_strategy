8a657a7aa60caeebf0c3a6cf748fbd1a7a110eb2
jczheng <jczheng198508@gmail.com>
2021-07-06 22:49:58 +0800

    promising novel quick stop loss method

--- vegas_strategy_once.py
+++ vegas_strategy_once.py
@@ -93,7 +93,7 @@ currency_df = pd.read_csv(currency_file)
 
 #currency_df = currency_df[currency_df['currency'].isin(['EURNZD', 'EURJPY', 'EURCHF', 'GBPCAD', 'AUDCAD'])]
 
-#currency_df = currency_df[currency_df['currency'].isin(['CHFJPY'])]
+#currency_df = currency_df[currency_df['currency'].isin(['EURGBP'])]
 
 
 # print("currency_df:")
@@ -428,7 +428,7 @@ if True:
 
     #dest_folder = "C:\\Users\\User\\Dropbox\\forex_real_time_new2_improve_filter_vegas_guppy_other_side_fixBug_15"
 
-    dest_folder = "C:\\Forex\\new_experiments\\0704\\forex_real_time_new2_improve_filter_vegas_guppy_other_side_fixBug_15_urgentStopLoss200_quick2_changeTo20"
+    dest_folder = "C:\\Forex\\new_experiments\\0705\\forex_real_time_new2_improve_filter_vegas_guppy_other_side_fixBug_15_urgentStopLoss200_quick2_stopLossImprove"
 
     #dest_folder = "C:\\Forex\\new_experiments\\0627\\not_support_half_close"
 
