cdfaed17765cd6a596bbb35fd58b4e32b9a07071
jczheng <jczheng198508@gmail.com>
2021-07-12 10:06:42 +0800

    what

--- vegas_strategy_once.py
+++ vegas_strategy_once.py
@@ -93,7 +93,7 @@ currency_df = pd.read_csv(currency_file)
 
 #currency_df = currency_df[currency_df['currency'].isin(['EURNZD', 'EURJPY', 'EURCHF', 'GBPCAD', 'AUDCAD'])]
 
-#currency_df = currency_df[currency_df['currency'].isin(['GBPJPY'])]
+currency_df = currency_df[currency_df['currency'].isin(['GBPUSD'])]
 
 
 # print("currency_df:")
@@ -421,14 +421,14 @@ sendEmail("Trader process ends", "")
 
 print("Finished trading *********************************")
 
-if True:
+if False:
     print("Sleeping")
     time.sleep(10)
     #dest_folder = "C:\\Users\\User\\Dropbox\\forex_real_time_new4_check_2barContinuous"
 
     #dest_folder = "C:\\Users\\User\\Dropbox\\forex_real_time_new2_improve_filter_vegas_guppy_other_side_fixBug_15"
 
-    dest_folder = "C:\\Forex\\new_experiments\\0711\\forex_real_time_new2_improve_filter_vegas_guppy_other_side_fixBug_15_urgentStopLoss200_quick2_innovativeFire2_reentry"
+    dest_folder = "C:\\Forex\\new_experiments\\0712\\forex_real_time_new2_improve_filter_vegas_guppy_other_side_fixBug_15_urgentStopLoss200_quick2_innovativeFire2"
 
     #dest_folder = "C:\\Forex\\new_experiments\\0627\\not_support_half_close"
 
