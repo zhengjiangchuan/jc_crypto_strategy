ec9c0bcd065c4225847734246cbb42f6229347f5
jczheng <jczheng198508@gmail.com>
2023-06-10 17:02:40 +0800

    update

--- vegas_strategy_once.py
+++ vegas_strategy_once.py
@@ -120,9 +120,9 @@ def start_do_trading():
 
     is_gege_server = False
 
-    is_real_time_trading = True
+    is_real_time_trading = False
 
-    is_weekend = False
+    is_weekend = True
 
     is_do_portfolio_trading = False
 
@@ -191,7 +191,7 @@ def start_do_trading():
     performance_files = []
 
     #chart_folder_name = "chart_ratio1Adjust_USDCAD2_newStuff_April_EURJPY2_noConsecutive_0512_correct2_filter"
-    chart_folder_name = "chart_ratio1Adjust_0512_correct2_filter2_realTime_w2_erase2_new_back3"
+    chart_folder_name = "chart_ratio0.5Adjust_0512_correct2_filter2_realTime_w2_erase2_new_back3"
     for currency_pair in currency_pairs:
 
         currency = currency_pair.currency
