c9ac3983c17485187891e35b7c0587bc26f0cacc
unknown <jczheng198508@gmail.com>
2024-04-14 12:27:38 +0800

    change end date

--- FindOptimalPortfolio.py
+++ FindOptimalPortfolio.py
@@ -731,7 +731,7 @@ def calculate_currency_performance(end_date, currency_list, sorted, accumulated_
 
 
 start_dates = [datetime(2023,4,1)]
-end_dates = [datetime(2024,4,5)]
+end_dates = [datetime(2024,4,16)]
 
 columns = ['by_date', 'optimal_currency_list']
 final_data = []
