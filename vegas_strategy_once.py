eb5219acce072eb5785a23720a7d753568d3a48c
unknown <jczheng198508@gmail.com>
2024-04-14 14:50:06 +0800

    update

--- vegas_strategy_once.py
+++ vegas_strategy_once.py
@@ -56,7 +56,7 @@ currency_to_run = options.currency_pair
 
 app_id = "168180645499516"
 
-use_dynamic_TP = False
+use_dynamic_TP = True
 
 profit_loss_ratio = 1
 
@@ -319,9 +319,9 @@ def start_do_trading():
 
     data_source = 2 if is_crypto else 1
 
-    is_real_time_trading = True
+    is_real_time_trading = False
 
-    is_weekend = False
+    is_weekend = True
 
     is_do_portfolio_trading = False
 
