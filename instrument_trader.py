afa288715ebfd59b4ec345b0f43b25318aff3502
unknown <jczheng198508@gmail.com>
2024-04-21 18:32:48 +0800

    typo

--- instrument_trader.py
+++ instrument_trader.py
@@ -3027,7 +3027,7 @@ class CurrencyTrader(threading.Thread):
 
                                 current_time = str(self.data_df.iloc[-1]['time'] + timedelta(hours=1))
 
-                                message_title =use "Short position of " + self.currency + " hits stop profit"
+                                message_title = "Short position of " + self.currency + " hits stop profit"
 
                                 message = "At " + current_time + ", the price of " + self.currency + " reaches next profit level " + " TP" + str(tp_number) + " " + str(self.round_price(short_target_profit_price - unit_range)) + "\n"
                                 message += "It yeilds a profit of " + str(round(tp_number * unit_loss / 2.0)) + " HK dollars"
