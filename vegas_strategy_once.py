fb395d6b65bd4902a72c6b0e7e762d03c8c4802f
zhengjiangchuan <jczheng198508@gmail.com>
2024-09-26 21:32:48 +0800

    update

--- vegas_strategy_once.py
+++ vegas_strategy_once.py
@@ -327,9 +327,9 @@ def start_do_trading():
 
     #data_source = 2
 
-    is_real_time_trading = False
+    is_real_time_trading = True
 
-    is_weekend = True
+    is_weekend = False
 
     is_do_portfolio_trading = False
 
