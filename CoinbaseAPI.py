0a6e980dd24e10c27c6d3d80fe091e0df67fb1bb
zhengjiangchuan <jczheng198508@gmail.com>
2025-05-17 20:29:23 +0800

    try

--- CoinbaseAPI.py
+++ CoinbaseAPI.py
@@ -27,8 +27,7 @@ account = accounts.accounts[0]
 portfolio_id = account['retail_portfolio_id']
 print("portfolio_id = " + str(portfolio_id))
 
-
-
+#This is the correct one
 # products = client.get_products()
 #
 # print("Product")
